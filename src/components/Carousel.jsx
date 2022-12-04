5bf2a7880beb53ab38109480a7a9334588984016
Calicolive <calicodeep@gmail.com>
2022-12-03 20:08:13 -0500

    removed indicators

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -5,48 +5,41 @@ export const Carousel = () => {
         Current Promotions
       </div>
       <div className=' flex flex-col items-center justify-center space-y-4 lg:space-y-0 lg:flex-row lg:space-x-3'>
-        <div className='indicator'>
-          <span className='indicator-item badge badge-secondary'>FREE</span>
-          <div className='card w-96  bg-base-100 shadow-xl '>
-            <figure>
-              <img src='/images/SA2.png' alt='SA' />
-            </figure>
-            <div className='card-body'>
-              <h2 className='card-title'>luapselREVGUITAR</h2>
-              <p>Free sampler instrument!</p>
-              <div className='card-actions justify-end'>
-                <button className='btn btn-primary'>Download</button>
-              </div>
+        <div className='card w-96  bg-base-100 shadow-xl '>
+          <figure>
+            <img src='/images/SA2.png' alt='SA' />
+          </figure>
+          <div className='card-body'>
+            <h2 className='card-title'>luapselREVGUITAR</h2>
+            <p>Free sampler instrument!</p>
+            <div className='card-actions justify-end'>
+              <button className='btn btn-primary'>Download</button>
             </div>
           </div>
         </div>
-        <div className='indicator'>
-          <span className='indicator-item badge badge-secondary'>50% off!</span>
-          <div className='card w-96 bg-base-100 shadow-xl '>
-            <figure>
-              <img src='/images/sa3.png' alt='Shoes' />
-            </figure>
-            <div className='card-body'>
-              <h2 className='card-title'>Oscilloscope</h2>
-              <p>Intro pricing 50% off!</p>
-              <div className='card-actions justify-end'>
-                <button className='btn btn-primary'>Buy Now</button>
-              </div>
+
+        <div className='card w-96 bg-base-100 shadow-xl '>
+          <figure>
+            <img src='/images/sa3.png' alt='Shoes' />
+          </figure>
+          <div className='card-body'>
+            <h2 className='card-title'>Oscilloscope</h2>
+            <p>Intro pricing 50% off!</p>
+            <div className='card-actions justify-end'>
+              <button className='btn btn-primary'>Buy Now</button>
             </div>
           </div>
         </div>
-        <div className='indicator'>
-          <span className='indicator-item badge badge-secondary'>FREE</span>
-          <div className='card  w-96 bg-base-100 shadow-xl'>
-            <figure>
-              <img src='/images/SA.png' alt='Shoes' />
-            </figure>
-            <div className='card-body'>
-              <h2 className='card-title'>healingBOWL</h2>
-              <p>Free sampler instrument!</p>
-              <div className='card-actions justify-end'>
-                <button className='btn btn-primary'>Download</button>
-              </div>
+
+        <div className='card  w-96 bg-base-100 shadow-xl'>
+          <figure>
+            <img src='/images/SA.png' alt='Shoes' />
+          </figure>
+          <div className='card-body'>
+            <h2 className='card-title'>healingBOWL</h2>
+            <p>Free sampler instrument!</p>
+            <div className='card-actions justify-end'>
+              <button className='btn btn-primary'>Download</button>
             </div>
           </div>
         </div>
